3e03770fd2fb92dc3443d2d1c7748cce5297f6bd
Craig <craigvu1@gmail.com>
2017-05-15 22:45:58 +0200

    Update settings-sidebar.php
    changed Drip form

--- templates/settings-sidebar.php
+++ templates/settings-sidebar.php
@@ -1,31 +1,29 @@
 <div id="ssp-sidebar">
 	<div class="sidebar-content">
-		<img src="<?php echo $image_dir ?>pm-connect-knot.jpg" src="https://www.podcastmotor.com/connect">
-		<form action="https://www.getdrip.com/forms/49904156/submissions" method="post" data-drip-embedded-form="49904156">
+		<img src="<?php echo $image_dir ?>pm-connect-knot.jpg" src="https://www.seriouslysimplepodcasting.com/hosting">
+		<form action="https://www.getdrip.com/forms/38739479/submissions" method="post" data-drip-embedded-form="38739479">
 			<h1>Get 20% off</h1>
 			<h4>Drop in your name and email and we’ll send you a coupon for 20% off your subscription to Seriously Simple Hosting.</h4>
 			
 			<!-- form data -->
-			<h3 data-drip-attribute="headline">Seriously Simple Hosting Discount</h3>
-			<div data-drip-attribute="description"></div>
-			<div>
-				<label for="fields[first_name]">First Name</label><br/>
-				<input type="text" name="fields[first_name]" value=""/>
-			</div>
-			<div>
-				<label for="fields[last_name]">Last Name</label><br/>
-				<input type="text" name="fields[last_name]" value=""/>
-			</div>
-			<div>
-				<label for="fields[email]">Email Address</label><br/>
-				<input type="email" name="fields[email]" value=""/>
-			</div>
-			<div>
-				<input type="submit" name="submit" value="Send me the coupon" data-drip-attribute="sign-up-button"/>
-			</div>
-			
-			<p>Spam sucks. We will not use your email for anything else and you can unsubscribe with just a click, anytime.</p>
-		</form>
+			  <h3 data-drip-attribute="headline">Seriously Simple Hosting Discount</h3>
+  			<div data-drip-attribute="description"></div>
+    				<div>
+        				<label for="fields[first_name]">First Name</label><br />
+        				<input type="text" name="fields[first_name]" value="" />
+    				</div>
+    				<div>
+        				<label for="fields[last_name]">Last Name</label><br />
+        				<input type="text" name="fields[last_name]" value="" />
+    				</div>
+    				<div>
+        				<label for="fields[email]">Email Address</label><br />
+        				<input type="email" name="fields[email]" value="" />
+    				</div>
+  			<div>
+    <input type="submit" name="submit" value="Sign Up" data-drip-attribute="sign-up-button" />
+  </div>
+</form>
 	
 	</div>
 </div>
